adfd751f213c0d0e370721f0cbb43e8980389065
Godwin Olorunshola <gtwatt1001@gmail.com>
2019-09-30 17:08:06 +0100

    exposed the api for getting slider and movie for day

--- main.go
+++ main.go
@@ -4,49 +4,71 @@ import (
     "fmt"
     "log"
     "net/http"
-
-    "github.com/PuerkitoBio/goquery"
+    "github.com/gorilla/mux"
+	"github.com/PuerkitoBio/goquery"
+	"encoding/json"
+	"strings"
 )
 
 type Movies []Movie
+
 func main() {
-    // Make HTTP request
-    response, err := http.Get("https://vivacinemas.com")
+   
+	// getHomeSliders( document)
+	// getMovieForDay( "Mon", document)
+	r := mux.NewRouter()
+	r.HandleFunc("/", handleHomeSlidersReq)
+	r.HandleFunc("/movies/{day}", handleMovieForDay)
+	log.Fatal(http.ListenAndServe(":5000", r))
+}
+
+func handleMovieForDay(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("content-type", "application/json")
+	params := mux.Vars(r)
+	day := params["day"]
+	json.NewEncoder(w).Encode(getMovieForDay(day))
+
+} 
+
+func handleHomeSlidersReq(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("content-type", "application/json")
+	json.NewEncoder(w).Encode(getHomeSliders())
+}
+
+func getDom() (*goquery.Document){
+	response, err := http.Get("https://vivacinemas.com")
     if err != nil {
         log.Fatal(err)
     }
     defer response.Body.Close()
-
-    // Create a goquery document from the HTTP response
-    document, err := goquery.NewDocumentFromReader(response.Body)
+	document, err := goquery.NewDocumentFromReader(response.Body)
     if err != nil {
         log.Fatal("Error loading HTTP response body. ", err)
-    }
-	// getHomeSliders( document)
-	getMovieForDay( "Mon", document)
-
-
-	
+	}
+	return document
 }
 
-func getHomeSliders(document *goquery.Document){
+
+func getHomeSliders() ( Movies){
+	document := getDom()
 	var movies Movies
 	document.Find(".movie-slide").Each(func(index int, element *goquery.Selection) {
 		imgSrc, exists := element.Find("img").Attr("data-lazy-src")
 		title := element.Find("h4").Text()
 
 		if exists {
-			movies = append(movies, Movie{Title : title, Image : imgSrc, Genre : "",Duration : ""})
-
+			movies = append(movies, Movie{Title : title, Image : imgSrc, Genre : "",Duration : "", Link : ""})
 		}else{
 			fmt.Println("imgSrc")
 		}
 		fmt.Println(movies)
 
 	})
+	return movies
 }
 
-func getMovieForDay(day string ,document *goquery.Document){
+func getMovieForDay(day string) (Movies){
+	document := getDom()
 	var movies Movies
 
 	valueToSearch := fmt.Sprintf("#%s .row.movie-tabs" ,day)
@@ -56,15 +78,15 @@ func getMovieForDay(day string ,document *goquery.Document){
 		genre := element.Find(".title").Text()
 		title := element.Find(".no-underline").Text()
 		duration := element.Find(".running-time ").Text()
+		link, _ := element.Find("a").Attr("href")
 
 		if exists {
-			movies = append(movies, Movie{Title : title, Image : imgSrc, Genre : genre, Duration: duration})
+			movies = append(movies, Movie{Title : title, Image : imgSrc, Genre : strings.TrimSpace(genre), Duration: strings.TrimSpace(duration), Link: link})
 		}else{
 			fmt.Println("Not found")
 		}
 	})
-	fmt.Println(movies)
-
+	return movies
 }
 
 
@@ -73,4 +95,6 @@ type Movie struct{
 	Image string `json:"image"`
 	Genre string `json:"genre"`
 	Duration string `json:"duration"`
+	Link string `json:"link"`
+
 }
